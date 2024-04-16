68e82c82755049f18e5a271d0d3f262cc5b95ea1
Adilfarooque <Adilfarq11@gmail.com>
2024-04-16 16:25:47 +0530

    Update

--- pkg/models/product.go
+++ pkg/models/product.go
@@ -5,6 +5,6 @@ type Product struct {
 	Name             string           `json:"name"`
 	Stock            int64           `json:"stock"`
 	Price            int64          `json:"price"`
-	StockDecreaseLog StockDecreaseLog `gorm:"foreginkey:ProductRefer"`
+	StockDecreaseLogs StockDecreaseLog `gorm:"foreignKey:ProductRefer"`
 }
 
